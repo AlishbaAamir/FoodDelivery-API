cf3b5243abaa297b28dc41dc2d9cfe094ab1025b
Alishba Aamir <amiralishba040@gmail.com>
2023-08-02 00:48:49 +0500

    UASER_API

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const port = process.env.SERVER_PORT || 9000
 
 app.use(express.json())
-
+ app.use('/api', require('./api/User/Router'))
  app.use('/api', require('./api/Category/Router'))
  app.use('/api', require('./api/Restuarent/Router'))
  app.use('/api', require('./api/Items/Router'))
