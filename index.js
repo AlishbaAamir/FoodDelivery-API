eeab311fd7db8968011f089c93c908e82d2386fb
Alishba Aamir <amiralishba040@gmail.com>
2023-08-14 23:45:57 +0500

    API

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const port = process.env.SERVER_PORT || 9000
+const cors = require('cors');
 
 app.use(express.json())
+app.use (cors({
+  origin: [' http://localhost:5173/'],
+  credentials : true,
+}));
+
  app.use('/api', require('./api/User/Router'))
  app.use('/api', require('./api/Category/Router'))
  app.use('/api', require('./api/Restuarent/Router'))
